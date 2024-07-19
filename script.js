af78236e4d20cced57ec6a14d9fd5c5fb41e231a
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-19 22:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let findLength=(str)=>{
+	let n=str.length;
+	return n;
+}
+let urlLength = findLength(window.location.href);
+alert("The length of the URL is: " + urlLength);
